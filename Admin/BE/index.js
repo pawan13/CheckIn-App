809a67e1310853baff7e56e85000f8583ace8b50
Pawan Kumar Siwakoti <pawanshiwakoti@Pawans-MacBook-Pro.local>
2024-04-07 11:33:33 +1000

    more fixes

--- Admin/BE/index.js
+++ Admin/BE/index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 //Basic Middleware
 app.use(cors());
