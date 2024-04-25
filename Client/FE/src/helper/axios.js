4cf0078d2741b9540e7265b5bd8d08673ed1f286
Kiran Gautam <ukiras@gmail.com>
2024-04-25 14:23:25 +1000

    up

--- Client/FE/src/helper/axios.js
+++ Client/FE/src/helper/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Base_URL = "http://localhost:3001/api/v1";
+const Base_URL = "http://client-api:3001/api/v1";
 
 const axiosProcessor = async ({ method, url = {}, body }) => {
   try {
